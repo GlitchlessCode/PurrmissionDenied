a425d107678f95eec2a99539dc0c8dbbfabfac75
znorth1 <znorth@ualberta.ca>
2025-12-07 20:20:03 -0700

    Settings on title screen

--- PurrmissionDenied/Assets/Scripts/SettingsController.cs
+++ PurrmissionDenied/Assets/Scripts/SettingsController.cs
@@ -5,7 +5,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SettingsController : MonoBehaviour
+public class SettingsController : Subscriber
 {
     [SerializeField]
     private Text musicSliderText = null;
@@ -16,15 +16,49 @@ public class SettingsController : MonoBehaviour
     [SerializeField]
     private float maxSliderAmount = 100.0f;
 
+    [Header("Sliders")]
+    public Slider musicSlider;
+    public Slider soundSlider;
+
     [Header("Audio")]
     public Audio SoundSample;
 
     [Header("Events")]
-    public FloatGameEvent ChangeMusic;
-    public FloatGameEvent ChangeSound;
     public AudioGameEvent SoundSampleBus;
 
+    [Header("Event Transponders")]
+    public FloatGameEvent ChangeMusic;
+    public FloatGameEvent ChangeSound;
+
     private bool shouldPlaySample = true;
+
+    public override void Subscribe()
+    {
+        ChangeMusic?.Subscribe(OnChangeMusic);
+        ChangeSound?.Subscribe(OnChangeSound);
+    }
+
+    public void OnChangeMusic(float value)
+    {
+        if (musicSliderText != null)
+        {
+            float localValue = value * 100;
+            musicSliderText.text = localValue.ToString("0");
+            musicSlider.value = value * 100;
+        }
+        ChangeMusic?.Unsubscribe(OnChangeMusic);
+    }
+
+    public void OnChangeSound(float value)
+    {
+        if (soundSliderText != null)
+        {
+            float localValue = value * 100;
+            soundSliderText.text = localValue.ToString("0");
+            soundSlider.value = value * 100;
+        }
+        ChangeSound?.Unsubscribe(OnChangeSound);
+    }
 
     public void MusicValueChange(float value)
     {
